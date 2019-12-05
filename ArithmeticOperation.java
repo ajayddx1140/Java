339443a2c8000a84c7ef58e6930fb1a6b1153646
Ashish Mathur <ashishmathur.192@gmail.com>
2019-12-05 23:21:25 +0530

    Java Program to perform all arithmatic operations

---
+++ ArithmeticOperation.java
@@ -0,0 +1,24 @@
+package Mypack;
+
+public class ArithmeticOperation
+{
+	public double Sum(double x,double y)
+	{
+		return(x+y);
+		}
+	
+	public double Sub(double x,double y)
+	{
+		return(x-y);
+		}
+	
+	public double Multi(double x,double y)
+	{
+		return(x*y);
+		}
+		
+	public double Div(double x,double y)
+	{
+		return(x/y);
+		}
+}
