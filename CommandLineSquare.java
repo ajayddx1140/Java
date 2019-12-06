35c1e007ce20aedab260dbaea9493bc05bc11cff
Ashish Mathur <ashishmathur.192@gmail.com>
2019-12-06 22:54:00 +0530

    Java Program to take input from command line and sum sqaure of two number

---
+++ CommandLineSquare.java
@@ -0,0 +1,15 @@
+class CommandLineSquare
+{
+public static void main(String s[])
+	{
+	int n1,n2,sum;
+	n1=Integer.parseInt(s[0]);
+	n2=Integer.parseInt(s[1]);
+	System.out.println("Square of number 1 is :"+(n1*n1)+"\nSquare of number 2 is :"+(n2*n2));
+	sum=n1+n2;
+	System.out.println("Sum of two numbers is :"+sum);
+	System.out.println("Square of Sum of two numbers is :"+(sum*sum));
+	
+	}
+	}
+km
