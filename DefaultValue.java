f62762808fb58f9d8e53451b1c725fd85d996c68
Ashish Mathur <ashishmathur.192@gmail.com>
2019-12-17 23:44:06 +0530

    Java program to show default values of all primitive data types

---
+++ DefaultValue.java
@@ -0,0 +1,24 @@
+class DefaultValue
+{	
+
+	static byte b;
+	static short sh;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static boolean bool;
+	
+	public static void main(String s[])
+	{
+		System.out.println("Byte :"+b);
+		System.out.println("Short :"+sh);
+		System.out.println("Int :"+i);
+		System.out.println("Long :"+l);
+		System.out.println("Float :"+f);
+		System.out.println("Double :"+d);
+		System.out.println("Char :"+c);
+		System.out.println("Boolean :"+bool);
+	}
+}
