651eea2952ea2fc47c918facb22baa99f9b87893
Ashish Mathur <ashishmathur.192@gmail.com>
2020-01-20 23:36:26 +0530

    Java Program to check Vowel/Consonent

---
+++ StoreVowel.java
@@ -0,0 +1,15 @@
+class StoreVowel
+{
+public static void main(String s[])
+{
+	Vowel(new char[]{'A','E','I','O','U'});
+}
+	
+	public static void Vowel(char v[])
+	{
+	for(char a:v)
+	{
+	System.out.println(a);
+	}
+	}
+	}
