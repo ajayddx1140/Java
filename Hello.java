a705faf5cf18347d317307f44eac2275ff855c7b
sameer3110 <72165763+sameer3110@users.noreply.github.com>
2020-10-01 01:55:05 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -9,9 +9,12 @@ public static void main(String s[])
 	for(int i = 0; i < x.length; i += 2)
 		for(int j = y.length-1; j > 0; j--)
 			if(((i+j) % 2) == 0)
+				System.out.println("X element is called :");
 				System.out.println(x[i]);
 			else
+				System.out.println("Y element is called :");
 				System.out.println(y[j]);
+				
 			
 }
 }
